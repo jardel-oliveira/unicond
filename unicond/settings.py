417022fcad8ac94b95d5d2e904ca6810efee6736
Jardel <jmkt@gmx.us>
2019-06-17 19:19:17 -0300

    update requerements.txt

--- unicond/settings.py
+++ unicond/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -73,12 +76,15 @@ WSGI_APPLICATION = 'unicond.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
 
 
 # Password validation
