e6b044a4e8422d9afbe1914e9e6765a1cfdd0cbc
jardel-oliveira <jardel.silva.oliveira@gmail.com>
2023-09-13 09:31:54 -0300

    up files

---
+++ staticfiles/js/style.js
@@ -0,0 +1,24 @@
+$("document").ready(function(){
+    $(".button-collapse").sideNav();
+  });
+
+// Caroussel Inicial
+
+$('.carousel.carousel-slider').carousel({
+fullWidth: true,
+indicators: true
+});
+
+
+// opções
+
+
+$(document).ready(function(){
+  $('select').formSelect();
+});
+
+
+$(document).ready(function(){
+  $('.tooltipped').tooltip();
+});
+      
--- unicond/settings.py
+++ unicond/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tf8l(ubpbz%r*koz@3%64qun$bn&!quicly#+dc-*3ju^jrcpe'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
